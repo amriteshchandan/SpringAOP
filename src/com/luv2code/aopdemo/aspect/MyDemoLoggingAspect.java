97f5b2c8b3926d966d3dbe7642b27dd3072041e2
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-29 17:02:36 +0530

    @Order - Ordering Aspects

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(value = 2)
 public class MyDemoLoggingAspect {
 
 	@Before(value = "com.luv2code.aopdemo.aspect.AOPAspectsPointCutExpression.forDAOPackageNoGettersAndSetters()")
